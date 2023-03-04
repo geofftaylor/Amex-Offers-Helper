ca251c4b7047dc2f2537d528a61a21e1842c37f6
Geoff Taylor <geoff@taylors.name>
2023-03-04 11:11:11 -0500

    Update to click only “Add to Card” buttons
    … and unwrap main function so it can be run as a bookmarklet.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function main() {
-	const WAIT_TIME_MS = 2000;
-	const offerButtons = document.querySelectorAll('button.btn.offer-cta');
-	for (let i = 0; i < offerButtons.length; ++i) {
-		setTimeout(() => {
-			try {
-				console.log('Clicking ' + offerButtons[i].id);
-				offerButtons[i].click();
-			} catch (e) {
-				console.error(e);
-			}
-		}, i * WAIT_TIME_MS)
-	}
-};
-
-main();
+const WAIT_TIME_MS = 2000;
+// The buttons we care about have class "offer-cta" and title "Add to Card."
+// The other buttons, e.g., "Add a Member," don't have the "Add to Card" title.
+const offerButtons = document.querySelectorAll('button.offer-cta[title="Add to Card"]');
+for (let i = 0; i < offerButtons.length; ++i) {
+	setTimeout(() => {
+		try {
+			console.log('Clicking ' + offerButtons[i].id);
+			offerButtons[i].click();
+		} catch (e) {
+			console.error(e);
+		}
+	}, i * WAIT_TIME_MS)
+}
